98a550d1bde5bc57f176613427c5c542797384f8
김상민 <wrt123123123@gmail.com>
2021-04-15 14:59:29 +0900

    myportal SQL연결, insert호출

--- myportal/src/main/java/com/bitacademy/myportal/controller/MainController/GuestbookController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controller/MainController/GuestbookController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bitacademy.myportal.service.GuestbookService;
@@ -17,11 +20,26 @@ public class GuestbookController {
 	@Autowired
 	GuestbookService guestbookServiceImpl;
 	
-	@ResponseBody
+//	@ResponseBody
 	@RequestMapping({"", "/", "/list"})
-	//	-> /guestbook/, /guestbook/ , /guestbook/list
-	public String list() {
+	//	-> /guestbook , /guestbook/ , /guestbook/list
+	public String list(Model model) {
 		List<GuestbookVo> list = guestbookServiceImpl.getList();
-		return list.toString();
+		//	데이터를 모델에 추가
+		model.addAttribute("list", list);
+		return "guestbook/list";
 	}
+	
+	//	게시물 작성 메서드
+	@RequestMapping(value="/write",
+			method=RequestMethod.POST)
+	public String write(@ModelAttribute GuestbookVo vo) {
+		System.out.println("VO:" + vo);
+		boolean success = guestbookServiceImpl.writeMessage(vo);
+		System.out.println("Write Result:" + success);
+		
+		//	리스트 페이지로 리다이렉트
+		return "redirect:/guestbook";
+	}
+	
 }
--- myportal/src/main/java/com/bitacademy/myportal/repository/GuestbookDaoImpl.java
+++ myportal/src/main/java/com/bitacademy/myportal/repository/GuestbookDaoImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bitacademy.myportal.vo.GuestbookVo;
@@ -12,24 +14,22 @@ import com.bitacademy.myportal.vo.GuestbookVo;
 //	-> guestbookDaoImpl 이름을 bean에 등록
 //	@Repository("guestbookDao")
 public class GuestbookDaoImpl implements GuestbookDao {
-
+	
+	@Autowired
+	SqlSession sqlSession;
+	
 	@Override
 	public List<GuestbookVo> selectAll() {
-		//	가상 데이터: 단순 텍스트 출력이라 한글은 ???로 표시된다
-		//		-> encoding이 적용되는 출력은 JSP파일에서 출력을 하면 된다
-		//	TODO: 실제 DB와 연결해보자
-		List<GuestbookVo> list = new ArrayList<>();
-		//	Long no, String name, String password, String content, Date regDate
-		list.add(new GuestbookVo(1L, "홍길동", "1234", "왔다갑니다", new Date()));
-		list.add(new GuestbookVo(2L, "장길산", "pass", "저두요", new Date()));
-		list.add(new GuestbookVo(3L, "전우치", "test", "반갑습니다", new Date()));
+		//	TODO: 예외전환 처리
+		List<GuestbookVo> list = sqlSession.selectList("guestbook.selectAll");
+		System.out.println("방명록: " + list);
 		return list;
 	}
 
 	@Override
 	public int insert(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		int insertedCount = sqlSession.insert("guestbook.insert", vo);
+		return insertedCount;
 	}
 
 	@Override
