88f124a8f0442d956d84b3fb4e7dc0300c872f4b
김상민 <wrt123123123@gmail.com>
2021-04-19 09:20:26 +0900

    board SQL, boardVo

--- myportal/SQL/board.sql
+++ myportal/SQL/board.sql
@@ -11,3 +11,22 @@ CREATE SEQUENCE seq_board_pk
     START WITH 1
     INCREMENT BY 1
     NOCACHE;
+    
+-- 실제 사용자 정보 확인
+SELECT * FROM members;
+
+-- 1
+INSERT INTO board
+    (no, title, content, hit, member_no)
+VALUES
+    (seq_board_pk.nextval,
+    '첫번째 게시물',
+    '첫번째 게시물입니다',
+    0,
+    1);
+    
+DESC board;
+
+SELECT * FROM board;
+
+commit;
